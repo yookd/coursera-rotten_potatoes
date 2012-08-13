d99f9938f790aca485f789cf60827c8fd96e0314
David  Yoo <dkyoo10@gmail.com>
2012-08-13 10:54:24 -0700

    Add movie filtering and sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,53 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+
+    # If new filter params are specified, save them in session
+    if params[:commit] == 'Refresh'
+      session[:ratings] = params[:ratings]
+    elsif params[:ratings] != session[:ratings]
+      # Save
+      params[:ratings] = session[:ratings]
+    end
+
+    # Check if we are sorting, save in session
+    if params[:sort]
+      session[:sort] = params[:sort]
+    elsif session[:sort]
+      # Save
+      params[:sort] = session[:sort]
+    end
+
+    # Store the ratings and sort for highlight and retaining checkboxes
+    @ratings = session[:ratings]
+    @sort = session[:sort]
+
+    # Check if session exists
+    if session[:ratings] && session[:sort]
+      @movies = Movie.find_all_by_rating(session[:ratings].keys, :order => session[:sort])
+    else
+      @movies = nil
+    end
+
+    #@movies = Movie.all
+
+    # if none of the ratings are checked, none should be displayed
+
+    # if filter -> filter the results
+    # store the ratings somehwere to display on the next view
+
+    # if sort -> see if there is anything set in the filter
+    # if there is, we sort the filter
+    # make sure we return the filter as well
+    
+    #@movies = Movie.find_all_by_rating(params[:ratings].keys).order('title') if params[:commit] == 'Refresh' && params[:sort] == 'title'
+    #@movies = Movie.find_all_by_rating(params[:ratings].keys, :order => 'release_date') if params[:commit] == 'Refresh' && params[:sort] == 'release_date'
+    #@movies = Movie.find_all_by_rating(params[:ratings].keys) if params[:commit] == 'Refresh' && params[:sort].nil?
+    #@movies = Movie.find_all_by_rating(params[:ratings].keys, :order => 'title') if params[:commit] == 'Refresh' && if params[:sort] == 'title'
+    #@movies = @movies.order('title') if params[:sort] == 'title'
+    #@movies = @movies.order('release_date') if params[:sort] == 'release_date'
+
+    @all_ratings = Movie.ratings
   end
 
   def new
